172c80c6c4fd476542dfa862297bc2be992c3631
Tyler Larkworthy <tyler.larkworthy@gmail.com>
2017-02-10 20:04:05 -0500

    Final Upload

---
+++ README.md
@@ -0,0 +1,13 @@
+## Instructions
+
+Begin typing a search term for a building on campus to see the results list dynamically updated as you type. Hit the enter key to force an update if it is slow. Results are listed in the order they appear in the API search results. Each result lists the title of the building, its address in parentheses (if available), and is a link to information about the building (as provided by the API).
+
+## Google Maps Integration
+
+The embedded Google map will display a search with the title of the building that is the first result as the search term. If no results are produced, it will default to searching for "University of Pennsylvania."
+
+## Thanks!
+
+This was actually a lot of fun to make. Thanks also to a few Penn Labs members (unfortunately I can't recall their names) for help with using JQuery to access the API and for clarifying other things for me.
+
+
--- buildings.js
+++ buildings.js
@@ -1,30 +1,58 @@
 
 
-var toplevel = {};
+var displayData = function displayData (data) {
 
-var getApiData = function getApiData (name) {
+    $('#results-body').remove(); //clear whatever search results were there previously
 
 
-    $.get('https://api.pennlabs.org/buildings/search?q=' + name, function (data) {
-        //console.log(body);
-        console.log(data);
-        toplevel = data;
-        //console.log(JSON.parse(body));
-        console.log(toplevel);
-        for (var i = 0; i < toplevel['result_data'].length; i++) {
-            console.log(toplevel['result_data'][i]['title']);
+    var $results = $('<div id="results-body">');
+    $results.append('<ul>');
+
+    //default in case no results are returned
+    var topTitle = "University+of+Pennsylvania";
+
+    for (var i = 0; i < data['result_data'].length; i++) {
+
+        topTitle = data['result_data'][0]['title']; //store name of first result
+
+        var title = data['result_data'][i]['title'];
+        var address = data['result_data'][i]['address'];
+        var link = data['result_data'][i]['http_link'];
+        var anchor = '<a href="' + link + '" target="_blank" >';
+
+        //only put parens if address exists
+        if (address.length > 0) {
+            address = ' (' + address + ')';
+        }
+        
+        //make sure link exists
+        if (link.length > 0) { 
+            title = anchor + title + address + '</a>';
         }
 
-    }, 'JSON');
+        $results.append('<li>' + title + '</li>');
+    }
+
+    $results.append('</ul>');
+    $('.results').append($results);
+
+
+    //update map with search for name of first result
+    $('iframe').attr('src','https://www.google.com/maps/embed/v1/search?key=AIzaSyC4HCSfVL66wCJA24BoYvQw8pCksc5cM0g&q='+ topTitle + '+in+Philadelphia');
 }
 
-
-var onClick = function onClick () {
-    $('.results').append('<p>Hello world</p>');
+var searchApi = function searchApi (name) {
+    $.get('https://api.pennlabs.org/buildings/search?q=' + name, displayData, 'JSON');
 }
-getApiData('hill');
 
-$('body').on('click', '.search-button', onClick);
+var update = function update (e) {
+    e.preventDefault();
+    searchApi($('#building-name').val());
+};
 
-//console.log(toplevel);
+
+$('form').on('submit', update);
+$('#building-name').on('keyup', update);
+
+
 
